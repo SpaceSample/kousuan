4db8d542e1beed1c0a9c8d07a23e1d29017eeadd
SpaceSample <wwjjxx0312@126.com>
2020-01-09 09:28:00 +0800

    Update build

---
+++ r/precache-manifest.5b5ba7f68bd40debdbbd080abaa69c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244b0d00eb4953e5e4f71879e6bedba6",
+    "url": "/kousuan/r/index.html"
+  },
+  {
+    "revision": "718611cb35fdc73f871c",
+    "url": "/kousuan/r/static/css/main.63408a33.chunk.css"
+  },
+  {
+    "revision": "75f603d8c725073cf404",
+    "url": "/kousuan/r/static/js/2.c2dcccf6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kousuan/r/static/js/2.c2dcccf6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "718611cb35fdc73f871c",
+    "url": "/kousuan/r/static/js/main.3453ca19.chunk.js"
+  },
+  {
+    "revision": "3cebec62b0c071c6684a",
+    "url": "/kousuan/r/static/js/runtime-main.b14d58a2.js"
+  }
+]);
--- r/service-worker.js
+++ r/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kousuan/r/precache-manifest.735842ff131b6787fbee4e1b40508840.js"
+  "/kousuan/r/precache-manifest.5b5ba7f68bd40debdbbd080abaa69c76.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kousuan/r/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
